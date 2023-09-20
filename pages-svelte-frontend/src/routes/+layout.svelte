9bfbbd542f7fbf392f541c8af9c3b6f337541dde
Christian Serra <castachris@hotmail.it>
2023-09-21 01:13:08 +0200

    feat(fe): added real time data

--- pages-svelte-frontend/src/routes/+layout.svelte
+++ pages-svelte-frontend/src/routes/+layout.svelte
@@ -1,11 +1,24 @@
 <script>
 	import '../app.postcss';
+
+	let date = new Date();
+	setInterval(() => {
+		date = new Date();
+	}, 1000);
+
 </script>
 
 <div class="hero min-h-screen bg-base-200">
 	<div class="hero-content text-center text-neutral-content">
 		<div class="">
 			<h1 class="mb-5 text-5xl font-bold">Catch Next Bus ATM</h1>
+			<div class="w-full flex justify-center mb-4">
+				<span class="countdown text-4xl">
+					<span style="--value:{date.getHours()};"></span>:
+					<span style="--value:{date.getMinutes()};"></span>:
+					<span style="--value:{date.getSeconds()};"></span>
+				</span>
+			</div>
 			<slot />
 		</div>
 	</div>
--- pages-svelte-frontend/src/routes/+page.svelte
+++ pages-svelte-frontend/src/routes/+page.svelte
@@ -2,10 +2,62 @@
 	import _ from 'lodash';
 	import type { PageData } from './$types';
 	import type { Estimate, LineStop } from '$lib/server/db/lineStops';
+	import { invalidateAll } from '$app/navigation';
 
 	export let data: PageData;
 
-	export const sortLines = (lines: LineStop[]) => _.orderBy(
+
+	const timerKey = (stop: string, lineIdx: number, estimateIdx: number) => stop + "|" + lineIdx + "|" + estimateIdx
+
+	const leaveWhen = (estimates: Estimate[]) => _.chain(estimates)
+			.filter('doable')
+			.orderBy('leaveHomeInMinutes')
+			.first()
+			.value()
+
+
+	type Timer = { minutes: number, seconds: number, pace: string }
+
+	const initTimers = () =>
+		Object.entries(data.lineStops).reduce(
+		(acc, [stop, lines]) => {
+			lines.forEach((line, i) => {
+				line.estimates?.forEach((estimates, j) => {
+					const estimate = leaveWhen(estimates);
+					if (!estimate) return;
+					const { leaveHomeInMinutes: minutes, type: pace } = estimate
+					acc[timerKey(stop,i,j)] = {minutes, seconds: 0, pace};
+				})
+			});
+			return acc;
+		}
+	,{} as { [key: string]: Timer });
+
+	let timers: { [key: string]: Timer } = initTimers();
+
+	setInterval(() => {
+		Object.values(timers).forEach((t) => {
+			t.seconds--;
+			if (t.seconds < 0) {
+				t.minutes--;
+				if (t.minutes < 0) {
+					t.minutes = 0;
+					t.seconds = 0;
+				}
+				else {
+					t.seconds = 59;
+				}
+			}
+		})
+		timers = timers;
+
+	}, 1000)
+
+	setInterval(() => {
+		invalidateAll().then(() => timers = initTimers())
+	}, 60000)
+
+	const sortLines = (lines: LineStop[]) => _.orderBy(
 		lines,
 		line => line.waitMessage?.includes("arrivo")
 			? 0
@@ -14,25 +66,12 @@
 				: Infinity
 		)
 
-	export const toTime = (date: string|Date) => {
+	const toTime = (date: string|Date) => {
 		const [hours, minutes] = new Date(date).toLocaleTimeString().split(":");
 		return hours + ":" + minutes;
 	}
 
-	export const reachable = (estimates: Estimate[]) => _.some(estimates, 'doable')
-	export const leaveWhen = (estimates: Estimate[]) => {
-		const estimate = _.chain(estimates)
-			.filter('doable')
-			.orderBy('leaveHomeInMinutes', 'desc')
-			.first()
-			.value()
-
-		if (!estimate) return "---";
-
-		const {leaveHomeInMinutes, type} = estimate;
-
-		return `Leave in ${leaveHomeInMinutes} min (${type})`
-	}
+	const notReachable = (estimates: Estimate[]) => _.every(estimates, ['doable', false])
 </script>
 
 <div class="flex flex-wrap flex-row justify-center">
@@ -44,24 +83,35 @@
 				</h2>
 				<div class="flex flex-col justify-between h-full">
 					<div class="card-actions justify-center">
-						{#each sortLines(lines) as line}
+						{#each sortLines(lines) as line, l}
 						<div class="justify-center w-full">
 							<h3 class="text-lg mb-2 divider">
 								{line.direction ? '↑' : '↓'}{line.lineId}: <b>{line.waitMessage ?? '---'}</b>
 							</h3>
-							{#each line.estimates ?? [] as estimates}
-								<div class="flex justify-end w-full">
-									<p class="flex ml-8" class:line-through={!reachable(estimates) && 'line-through'}>
-										{toTime(estimates[0].arrivesAt)}</p>
-									<p class="flex justify-end mr-8">{leaveWhen(estimates)}</p>
-								</div>
-							{/each}
+							<table class="table table-sm w-full">
+								<tbody>
+								{#each line.estimates ?? [] as estimates, e}
+									<tr class="!leading-3" class:opacity-20={notReachable(estimates)}>
+										<th>{toTime(estimates[0].arrivesAt)}</th>
+										<td>Leave in</td>
+										<td>
+											<span class="countdown">
+												<span style="--value:{timers[timerKey(stop, l, e)]?.minutes};"/>:
+												<span style="--value:{timers[timerKey(stop, l, e)]?.seconds};"/>
+											</span>
+										</td>
+										<td>{timers[timerKey(stop, l, e)]?.pace ?? "never"}</td>
+									</tr>
+								{/each}
+								</tbody>
+							</table>
+
 						</div>
 						{/each}
 					</div>
 					<div>
 						<p class="divider mb-2 mt-6">Time to reach</p>
-						<div class="card-actions justify-center">
+						<div class="card-actions justify-between">
 							{#each lines[0].minutesFromHome as velocity}
 								<p>{velocity.type}: {velocity.minutes} minutes</p>
 							{/each}
